49adbd60037f6d020f9d3e4628f2488a08247ed7
TerraVulccano <ilya.deryushev@gmail.com>
2021-11-03 18:23:25 +0300

    -*

--- src/test/java/tests/StudentRegistrationFormTests.java
+++ src/test/java/tests/StudentRegistrationFormTests.java
@@ -1,8 +1,6 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
